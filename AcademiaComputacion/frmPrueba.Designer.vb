3264b1133c3bf007a90df5782ca84555014f1b24
antonio1394 <antoniorivm1394@gmail.com>
2015-07-17 12:41:01 -0600

    [update] realizando pruebas en los repositorios

---
+++ AcademiaComputacion/frmPrueba.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmPrueba
+    Inherits Telerik.WinControls.UI.RadForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RadLabel1 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadTextBox1 = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadTextBox2 = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadTextBox3 = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadTextBox4 = New Telerik.WinControls.UI.RadTextBox()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RadLabel1
+        '
+        Me.RadLabel1.Location = New System.Drawing.Point(60, 19)
+        Me.RadLabel1.Name = "RadLabel1"
+        Me.RadLabel1.Size = New System.Drawing.Size(58, 18)
+        Me.RadLabel1.TabIndex = 0
+        Me.RadLabel1.Text = "RadLabel1"
+        '
+        'RadTextBox1
+        '
+        Me.RadTextBox1.Location = New System.Drawing.Point(42, 58)
+        Me.RadTextBox1.Name = "RadTextBox1"
+        Me.RadTextBox1.Size = New System.Drawing.Size(228, 20)
+        Me.RadTextBox1.TabIndex = 1
+        '
+        'RadTextBox2
+        '
+        Me.RadTextBox2.Location = New System.Drawing.Point(42, 112)
+        Me.RadTextBox2.Name = "RadTextBox2"
+        Me.RadTextBox2.Size = New System.Drawing.Size(227, 20)
+        Me.RadTextBox2.TabIndex = 2
+        '
+        'RadTextBox3
+        '
+        Me.RadTextBox3.Location = New System.Drawing.Point(42, 165)
+        Me.RadTextBox3.Name = "RadTextBox3"
+        Me.RadTextBox3.Size = New System.Drawing.Size(227, 20)
+        Me.RadTextBox3.TabIndex = 3
+        '
+        'RadTextBox4
+        '
+        Me.RadTextBox4.Location = New System.Drawing.Point(42, 213)
+        Me.RadTextBox4.Name = "RadTextBox4"
+        Me.RadTextBox4.Size = New System.Drawing.Size(226, 20)
+        Me.RadTextBox4.TabIndex = 4
+        '
+        'FrmPrueba
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(313, 294)
+        Me.Controls.Add(Me.RadTextBox4)
+        Me.Controls.Add(Me.RadTextBox3)
+        Me.Controls.Add(Me.RadTextBox2)
+        Me.Controls.Add(Me.RadTextBox1)
+        Me.Controls.Add(Me.RadLabel1)
+        Me.Name = "FrmPrueba"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.Text = "FrmPrueba"
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RadLabel1 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadTextBox1 As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadTextBox2 As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadTextBox3 As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadTextBox4 As Telerik.WinControls.UI.RadTextBox
+End Class
+
